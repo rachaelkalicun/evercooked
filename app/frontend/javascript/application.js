b52198ae0b9232ac47aad1fa4605ff6b4ed58731
Rachael <rhg108@gmail.com>
2024-12-15 15:14:38 -0500

    get trix and actiontext working again

--- app/frontend/javascript/application.js
+++ app/frontend/javascript/application.js
@@ -1,14 +1,5 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// import "@hotwired/turbo-rails";
-// import "controllers";
-
-// import "trix"
-// import "@rails/actiontext"
+import "trix"
+import "@rails/actiontext"
 
 // import LocalTime from "local-time";
 // LocalTime.start();
-// alert('Hello from Vite! Edit me in app/frontend/javascript/application.js')
-// import "@hotwired/turbo-rails"
-// import "controllers"
-
-// alert('Hello Rachael! Edit me in app/frontend/javascript/application.js')
